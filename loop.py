1f7ac60adac387a8dff1ce9a54c0cc86db36dc10
Herbert M. Turner <herbieturner@verizon.net>
2018-10-13 19:38:20 -0400

    Created files

---
+++ loop.py
@@ -0,0 +1,12 @@
+"""This program simulates every possible combination of scores and
+outputs them to possibilities.csv
+"""
+scores = [1,4,8]
+domains = [C,A,R,E,S]
+possibilities = []
+for x in range(5):
+    
+    for x in range(5):
+        for x in range(5):
+            for x in range(5):
+                for x in range(5):
