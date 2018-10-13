c3ef08c23bba2ee428aeb17c2319a2c17bc9e390
Herbert M. Turner <herbieturner@verizon.net>
2018-10-13 19:59:26 -0400

    Finished computing

--- loop.py
+++ loop.py
@@ -1,12 +1,18 @@
 """This program simulates every possible combination of scores and
-outputs them to possibilities.csv
+outputs them to possibilities.csv. There are 243 possibilities.
 """
+import csv
+
 scores = [1,4,8]
-domains = [C,A,R,E,S]
-possibilities = []
-for x in range(5):
-    
-    for x in range(5):
-        for x in range(5):
-            for x in range(5):
-                for x in range(5):
+Domains = ['C','A','R','E','S']
+possibilities = [Domains]
+for a in range(3): #C
+    for b in range(3): #A
+        for c in range(3): #R
+            for d in range(3): #E
+                for e in range(3): #S
+                    possibilities.append([scores[a],scores[b],scores[c],scores[d],scores[e]])
+with open('possibilities.csv',"w") as csvfile:
+    pwriter = csv.writer(csvfile, lineterminator='\n')
+    pwriter.writerows(possibilities)
+print(len(possibilities))
